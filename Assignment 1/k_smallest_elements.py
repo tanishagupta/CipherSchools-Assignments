a473e1053d4cb5159c5c406a6aae864c0a99ec77
tanishagupta <42850128+tanishagupta@users.noreply.github.com>
2021-02-17 23:31:22 +0530

    Update and rename kth_smallest.py to k_smallest_elements.py

--- Assignment 1/k_smallest_elements.py
+++ Assignment 1/k_smallest_elements.py
@@ -1,3 +1,17 @@
+# Code to find k smallest elements
+def kth(arr,k):
+    arr.sort()
+    for i in range(k):
+        print(arr[i])
+
+arr = [7, 10, 4, 3, 20, 15]
+k = 4
+kth(arr,k)
+
+
+
+
+# Code to find kth smallest element
 def kth(arr,n,k):
     arr.sort()
     for i in range(n):
@@ -10,3 +24,6 @@ arr = [7, 10, 4, 3, 20, 15]
 n = len(arr)
 k = 4
 print(kth(arr,n,k))
+
+
+
